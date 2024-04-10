bc6c70df3bc4a438478288c2b522bf55d778cd57
akira-cn <akira.cn@gmail.com>
2024-04-10 11:06:06 +0800

    fix footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -36,9 +36,9 @@ function Footer() {
           </div> */}
         </div>
         <div className=" h-[80px] flex items-center justify-center">
-          <h1 className="text-[20px] font-normal pt-[20px]">
+          <h1 className="text-[16px] font-normal pt-[20px]">
             {" "}
-            © Copyright 2024 , All Rights Reserved ｜ <a href="http://beian.miit.gov.cn/" target="_blank" rel="noreferrer">京ICP备2024060726号-1</a>
+            {isDesktopOrLaptop && '© Copyright 2024 , All Rights Reserved ｜ '}<a href="http://beian.miit.gov.cn/" target="_blank" rel="noreferrer">京ICP备2024060726号-1</a>
           </h1>
         </div>
       </section>
